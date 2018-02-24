ee34889ddb40f77a3cfcc1cd0e993513dd49a682
Azela <asela@leafycode.com>
2018-02-24 18:35:26 +0530

    fixed last Id return in mysql

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require("express");
 var app = express();
 var db = require("./db");
 
+var TestController = require("./test/TestController");
+app.use("/api/", TestController);
+
 var UserController = require("./user/UserController");
 app.use("/api/users", UserController);
 
--- user/UserController.js
+++ user/UserController.js
@@ -8,37 +8,62 @@ var bodyParser = require("body-parser");
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
-// test a user simply from database
-router.get("/test", function(req, res) {
-  res.json({ code: 200, status: "Test" });
-});
-
 // save a user to database
 router.post("/", (req, res) => {
   pool.getConnection((err, connection) => {
     if (err) {
-      res.json({ code: 100, status: "Error in connection database" });
+      res.json({ code: 100, message: "Error in connection database" });
     }
 
-    let password = req.body.email;
+    let email = req.body.email;
+    let password = req.body.password;
 
-    if (req.body.password) {
+    if (!password) {
       password = generator.generate({
         length: 8,
         numbers: true
       });
     }
 
+    //check whether email is already registered
     connection.query(
-      `INSERT INTO user(email,password) VALUES('${req.body.email}','${
-        password
-      }')`,
-      err => {
-        connection.release();
+      `SELECT * FROM user WHERE email='${email}'`,
+
+      (err, result) => {
         if (err) {
-          res.json({ code: 100, status: "Query failed in DB" });
+          res.json({ code: 100, message: "Connection issue" });
         } else {
-          res.json({ code: 200, status: "Success" });
+          if (result.length > 0) {
+            connection.release();
+            res.json({
+              code: 409,
+              message: `A user with email: ${email} already exists.`,
+              developerMessage: `User creation failed because the email: ${
+                email
+              } already exists.`
+            });
+          } else {
+            connection.query(
+              `INSERT INTO user(email,password) VALUES('${req.body.email}','${
+                password
+              }')`,
+
+              (err, result) => {
+                connection.release();
+                if (err) {
+                  res.json({ code: 400, message: "Email is not set" });
+                } else {
+                  res.json({
+                    code: 201,
+                    self: `http://localhost:8090/api/users/${result.insertId}`,
+                    email
+                  });
+                }
+
+                console.log(result);
+              }
+            );
+          }
         }
       }
     );
