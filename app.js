b8f3f1bd1fb15b55b8f906929970b15b5dd6626a
Mahesh <maheshd95@gmail.com"git config --global user.name Maheshd95;git config --global user.email 'maheshd95@gmail.com>
2018-02-24 21:56:23 +0530

    #8 Implement POST /faculties issue finished

--- app.js
+++ app.js
@@ -5,4 +5,7 @@ var db = require("./db");
 var UserController = require("./user/UserController");
 app.use("/api/users", UserController);
 
+var FacultyController = require("./faculty/FacultyController");
+app.use("/api/faculties", FacultyController);
+
 module.exports = app;
---
+++ faculty/FacultyController.js
@@ -0,0 +1,59 @@
+var express = require("express");
+var router = express.Router();
+const pool = require("../db");
+var router = express.Router();
+var bodyParser = require("body-parser");
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+// save a faculty to database
+router.post("/", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      res.json({ code: 100, status: "Error in connection database" });
+    }
+
+    let name = req.body.name;
+
+    connection.query(
+        `SELECT * FROM faculty WHERE name='${name}'`,
+
+        (err, result) => {
+          if (err) {
+            res.status(100).json({ message: "Error in mysql query" });
+          } else {
+            if (result.length > 0 && result !== null) {
+
+              connection.release();
+              res.status(409).json({
+                status: 409,
+                message: `A faculty with name: ${name} already exists.`,
+                developerMessage: `Faculty creation failed because the faculty name: ${
+                  name
+                } already exists.`
+              });
+            } else {
+        connection.query(
+            `INSERT INTO faculty (name) VALUES ('${name}')`,
+        err => {
+          connection.release();
+          if (err) {
+            res.json({ status: 100, message: "Query failed in DB" });
+          } else {
+            res.status(201).json({
+              self: `http://localhost:8090/api/faculties/${result.faculty_id}`,
+              name: name
+            });
+          }
+        }
+      );
+            }
+          }
+        }
+      );
+
+  });
+});
+
+module.exports = router;
